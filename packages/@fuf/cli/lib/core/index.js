3e145c42aa7e248b3d77ab5bb1f5a9e060f0c77a
hakuna-tata <610578197@qq.com>
2021-04-15 00:34:15 +0800

    feat(core): init core

---
+++ packages/@fuf/cli/lib/core/index.js
@@ -0,0 +1,7 @@
+class Fuf {
+  constructor(args) {
+    console.log(args);
+  }
+}
+
+module.exports = Fuf;
--- packages/@fuf/cli/lib/index.js
+++ packages/@fuf/cli/lib/index.js
@@ -1,7 +1,10 @@
 const os = require('os');
+const minimist = require('minimist');
 const checkNodeVersion = require('./prepare/checkNodeVersion');
 const checkPkgVersion = require('./prepare/checkPkgVersion');
 const checkEnv = require('./prepare/checkEnv');
+
+const Fuf = require('./core');
 
 const prepare = () => {
   checkNodeVersion(process.version);
@@ -11,6 +14,9 @@ const prepare = () => {
 
 function cli() {
   prepare();
+
+  const args = minimist(process.argv.slice(2));
+  new Fuf(args);
 }
 
 module.exports = cli;

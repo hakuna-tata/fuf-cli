b162862a3a55592f828682b6e5cb20b3265409a0
hakuna-tata <610578197@qq.com>
2021-04-16 02:21:34 +0800

    feat(log): 正常打印log

--- packages/@fuf/cli-utils/lib/logger.js
+++ packages/@fuf/cli-utils/lib/logger.js
@@ -1,6 +1,9 @@
 const chalk = require('chalk');
 
 module.exports = {
+  log: (msg) => {
+    console.log(chalk.green(msg));
+  },
   error: (msg) => {
     console.log(chalk.white.bgRed.bold('Error: ') + chalk.blue.bold(`${msg}`));
   }

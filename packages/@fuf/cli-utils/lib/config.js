fb31a67d6aa5fd0e39ae84c73936c870d519e30e
hakuna-tata <610578197@qq.com>
2021-05-05 15:19:13 +0800

    fix(proxy): 发布问题

--- packages/@fuf/cli-utils/lib/config.js
+++ packages/@fuf/cli-utils/lib/config.js
@@ -3,32 +3,32 @@ module.exports = {
   INTERNAL_COMMAND_PKG: {
     create: {
       pkgName: '@fuf/cli-create-command',
-      version: '1.0.1',
+      version: '1.0.3',
     },
     add: {
       pkgName: '@fuf/cli-add-command',
-      version: '1.0.1',
+      version: '1.0.3',
     }
   },
   INTERNAL_TEMPLATE_PKG: {
     vue2: {
       pkgName: '@fuf/cli-template-vue2',
-      version: '1.0.1',
+      version: '1.0.3',
       desc: 'Vue2 模板',
     },
     vue3: {
       pkgName: '@fuf/cli-template-vue3',
-      version: '1.0.1',
+      version: '1.0.3',
       desc: 'Vue3 模板',
     },
     node: {
       pkgName: '@fuf/cli-template-node',
-      version: '1.0.1',
+      version: '1.0.3',
       desc: 'Node 模板',
     },
     react: {
       pkgName: '@fuf/cli-template-react',
-      version: '1.0.1',
+      version: '1.0.3',
       desc: 'React 模板 (正在开发中......)',
     }
   },
